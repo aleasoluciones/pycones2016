6aaecff3cbf8cf9410ead853d27c46147c731945
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2016-10-06 15:40:03 +0200

    Improve test description

--- fizzbuzz_spec.py
+++ fizzbuzz_spec.py
@@ -2,12 +2,10 @@
 
 from expects import *
 
-from fizzbuzz import fizzbuzz
-
 
 with describe('Fizzbuzz'):
-    with context('when running the test'):
-        with it('says hello'):
+    with context('when saying hello to Foo'):
+        with it('says Hello to Foo'):
             name = 'Foo'
             
             result = say_hello(name)
