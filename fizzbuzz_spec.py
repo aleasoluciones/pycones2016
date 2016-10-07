9afc73fee654063d90558e15db70aa1c843b189a
Bob Belderbos <bbelderbos@users.noreply.github.com>
2016-10-07 19:43:58 +0200

    yield wins
    (fizzbuzz) [bbelderb@macbook pycones2016 (vio_bob)]$ python2.7 -m timeit -s "from fizzbuzz import fizzbuzz_yield" "fizzbuzz_yield()"
    1000000 loops, best of 3: 0.522 usec per loop
    (fizzbuzz) [bbelderb@macbook pycones2016 (vio_bob)]$ python2.7 -m timeit -s "from fizzbuzz import fizzbuzz" "fizzbuzz()"
    10000 loops, best of 3: 35 usec per loop

--- fizzbuzz_spec.py
+++ fizzbuzz_spec.py
@@ -9,12 +9,18 @@ results = {
     3: "Fizz",
     5: "Buzz",
    15: "FizzBuzz",
+   18: "Fizz",
+   19: 19,
+   20: "Buzz",
+   60: "FizzBuzz",
 }
  
 with describe('Fizzbuzz'):
     with context('when saying calling fizzbuzz'):
         with it('returns list of results'):
             result = fizzbuzz()
+            result_yield = list(fizzbuzz_yield())
             
             for counter, expected in results.items():
                 expect(result[counter-1]).to(equal(expected))
+                expect(result_yield[counter-1]).to(equal(expected))
